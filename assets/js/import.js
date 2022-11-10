067d75131600045ba6bd708c9b698ff4c67b071b
kaspr <daniel@dmskaspr.com>
2022-11-09 20:52:18 -0600

    Initial Upload

---
+++ assets/js/import.js
@@ -0,0 +1,36 @@
+(function(){
+	let alertText = `File upload failed. Please try again using a valid JSON file.`;
+	let file = document.getElementById(`file`);
+	let sectionNameInput = document.getElementById(`sectionNameInput`);
+
+	function onChange(e){
+		let reader = new FileReader();
+		reader.onload = onReaderLoad;
+		reader.readAsText(e.target.files[0]);
+	};
+
+	function onReaderLoad(e){
+		try{
+			closeDialog();
+			let allSections = JSON.parse(localStorage.allSections);
+			let newSectionName = sectionNameInput.value;
+			JSON.parse(e.target.result).forEach(x =>{
+				let newName = JSON.parse(x).sectionName;
+				let newButtonsList = JSON.stringify(JSON.parse(x).sectionButtons);
+				let sectionInfo = `{"sectionName": "${newName}","sectionButtons": ${newButtonsList}}`;
+				let newSection = JSON.parse(sectionInfo);
+				allSections.push(newSection);
+			});
+			localStorage.allSections = JSON.stringify(allSections);
+			loadSections();
+			loadSideNav();
+			location.reload();
+		}
+		catch{
+			alert(alertText);
+			location.reload();
+		}
+	};
+
+	file.addEventListener(`change`, onChange);
+}());
